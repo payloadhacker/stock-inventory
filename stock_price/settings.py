57a13126a8c7c1fb318fed5979e037b40fbfcaff
Hermes Trigs <allenroger451@gmail.com>
2025-09-29 09:23:18 +0100

    Fix SECRET_KEY to use python-decouple and add .env.example

--- stock_price/settings.py
+++ stock_price/settings.py
@@ -21,10 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("^s(ril4h2e%neq8tv$-zr#zla3i#hy!kxuno3p7el%u5%rhux+")
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config("DEBUG", default=False, cast=bool)
 
+SECRET_KEY = config("SECRET_KEY")  
+DEBUG = config("DEBUG", default=False, cast=bool)
 
 ALLOWED_HOSTS = [
     "stock-inventory-production-2d3a.up.railway.app",
